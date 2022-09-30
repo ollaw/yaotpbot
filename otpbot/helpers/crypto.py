b30bc98fc24b8db732f386b4d07c6086b09de0bc
Gabriele <ollagabriele@protonmail.com>
2022-09-30 23:55:00 +0000

    Resolve "Encrypt data on DynamoDB"

---
+++ otpbot/helpers/crypto.py
@@ -0,0 +1,27 @@
+import base64
+import os
+
+from dynamodb_encryption_sdk.delegated_keys.jce import JceNameLocalDelegatedKey
+from dynamodb_encryption_sdk.identifiers import EncryptionKeyType, KeyEncodingType
+from dynamodb_encryption_sdk.material_providers.wrapped import (
+    WrappedCryptographicMaterialsProvider,
+)
+
+from ..constant import CRYPTO_ENCRYPTION_ENV_VAR, CRYPTO_SIGN_ENV_VAR
+
+_encryption_key = JceNameLocalDelegatedKey(
+    key=base64.b64decode(os.getenv(CRYPTO_ENCRYPTION_ENV_VAR) or base64.b64encode(os.urandom(32))),
+    algorithm="AES",
+    key_type=EncryptionKeyType.SYMMETRIC,
+    key_encoding=KeyEncodingType.RAW,
+)
+_signing_key = JceNameLocalDelegatedKey(
+    key=base64.b64decode(os.getenv(CRYPTO_SIGN_ENV_VAR) or base64.b64encode(os.urandom(32))),
+    algorithm="HmacSHA512",
+    key_type=EncryptionKeyType.SYMMETRIC,
+    key_encoding=KeyEncodingType.RAW,
+)
+
+wrapped_material_provider = WrappedCryptographicMaterialsProvider(
+    wrapping_key=_encryption_key, unwrapping_key=_encryption_key, signing_key=_signing_key
+)
---
+++ otpbot/helpers/env.py
@@ -0,0 +1,49 @@
+import base64
+import logging
+import os
+
+import boto3
+
+from ..constant import CRYPTO_ENCRYPTION_ENV_VAR, CRYPTO_SIGN_ENV_VAR, TOKEN_ENV_VAR
+
+logger = logging.getLogger(__name__)
+
+
+def check_environments():
+    _check_token()
+    _check_credentials()
+    _check_keys()
+
+
+def _check_token():
+    if os.getenv(TOKEN_ENV_VAR) is None:
+        logger.error(f"Environment variable {TOKEN_ENV_VAR} must be set with valid Telegram Token.")
+        exit(-1)
+
+
+def _check_credentials():
+    sts = boto3.client("sts")
+    try:
+        sts.get_caller_identity()
+        logger.info("Valid AWS credentials found.")
+    except Exception:
+        logger.error("AWS credentials are not set or invalid.")
+        exit(-1)
+
+
+def _check_keys():
+    if None in [os.getenv(CRYPTO_ENCRYPTION_ENV_VAR), os.getenv(CRYPTO_SIGN_ENV_VAR)]:
+        logger.error(
+            f"Environment variables {CRYPTO_ENCRYPTION_ENV_VAR} and {CRYPTO_SIGN_ENV_VAR} must be set."
+            "\nYou can use following python script to generate one:\n\nimport os,base64\nbase64.b64encode(os.urandom(16)).decode('utf-8')"
+        )
+        exit(-1)
+    if (len(base64.b64decode(os.getenv(CRYPTO_ENCRYPTION_ENV_VAR))) not in [16, 24, 32]) or (
+        len(base64.b64decode(os.getenv(CRYPTO_SIGN_ENV_VAR))) not in [16, 24, 32]
+    ):
+        logger.error(
+            f"{CRYPTO_ENCRYPTION_ENV_VAR} or {CRYPTO_SIGN_ENV_VAR} has invalid length."
+            "\nValid length are 128,192 or 256 bits."
+            "\nYou can use following python script to generate one:\n\nimport os,base64\nbase64.b64encode(os.urandom(16)).decode('utf-8')"
+        )
+        exit(-1)
